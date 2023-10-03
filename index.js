a9f235b3e0d606a942323ba9a2492d1c9052cb7a
mrrsmburu <mrrsmburu@gmail.com>
2023-10-03 13:07:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = `Welcome to Flatbook, {currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+const shortGreeting = `Welcome, {currentUser.slice(0, 1)}!`
